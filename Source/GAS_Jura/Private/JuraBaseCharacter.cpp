17e127f86b08bc290ac14264f78b1bf347fcfd1e
wuhoujia <2633520645@qq.com>
2024-09-16 12:03:49 +0800

    remove invalid assets

---
+++ Source/GAS_Jura/Private/JuraBaseCharacter.cpp
@@ -0,0 +1,19 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "JuraBaseCharacter.h"
+
+AJuraBaseCharacter::AJuraBaseCharacter()
+{
+ 	// Set this character to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = false;
+
+}
+
+// Called when the game starts or when spawned
+void AJuraBaseCharacter::BeginPlay()
+{
+	Super::BeginPlay();
+	
+}
+
---
+++ Source/GAS_Jura/Public/JuraBaseCharacter.h
@@ -0,0 +1,22 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Character.h"
+#include "JuraBaseCharacter.generated.h"
+
+UCLASS()
+class GAS_JURA_API AJuraBaseCharacter : public ACharacter
+{
+	GENERATED_BODY()
+
+public:
+	// Sets default values for this character's properties
+	AJuraBaseCharacter();
+
+protected:
+	// Called when the game starts or when spawned
+	virtual void BeginPlay() override;
+
+};
